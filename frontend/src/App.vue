62497558d1663c9d47a9ec8c92000a6bef879232
QuangMinhPham <pminh7794@gmail.com>
2025-12-25 12:22:38 +0700

    6

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,9 +4,7 @@
 
 <template>
   <router-view v-slot="{ Component }">
-    <transition name="fade" mode="out-in">
       <component :is="Component" />
-    </transition>
   </router-view>
 </template>
 
